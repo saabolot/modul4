9a6c4fe2f15800a69b38b5e7e1a54e9b6052e626
saabolot <97003103+saabolot@users.noreply.github.com>
2022-01-06 22:26:10 +0100

    commit 2

--- Moduł 4.2 zadanie 1.py
+++ Moduł 4.2 zadanie 1.py
@@ -1,6 +1,3 @@
-"""
-Program sprawdza, czy podane słowo jest palindromem.
-"""
 slowo = input("Wpisz słowo, aby sprawdzić czy jest palindromem:")      
 def palindrom(slowo):
     if slowo == slowo[::-1]:
